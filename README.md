6bcf41c2d96eea14990df2a01d6257cc6f10e09c
Konstantin Weddige <konstantin@skathi.net>
2015-12-31 13:55:40 +0100

    Use JSON to define recipients

--- README.md
+++ README.md
@@ -10,8 +10,8 @@ Argumente:
  * -s [SUBJECT] Der Betreff der Nachrichten, Voreinstllung ist "Ein frohes neues Jahr YYYY" mit dem jeweils aktuellen Jahr
  * -f [SENDER] Die Abersenderadresse, Voreinstllung ist "ich@domain.tld"
  * -b [BODY] Die Datei mit der Textvorlage, Voreinstellung ist "body.txt"
- * -r [RECIPIENTS] Die Datei mit den Empfängern, Voreinstellung ist "recipients.txt"
- 
-body.txt ist eine einfache Textdatei. Es kann der Platzhalter {name} verwendet werden.
+ * -r [RECIPIENTS] Die Datei mit den Empfängern, Voreinstellung ist "recipients.json"
 
-In recipients.txt steht in jeder Zeile ein Empfänger. Die Zeilen beginnen mit einer E-Mailadresse gefolgt von einem Tabulator und dem Namen des Empfängers.
+Die Datei recipients.json definiert eine Liste an Empfängern. Jeder Empfänger benötigt die Felder "email" und "name".
+body.txt ist eine einfache Textdatei mit Platzhaltern in der Form {name}.
+Es können alle in recipients.json definierten Variablen verwendet werden.
--- silvestermails.py
+++ silvestermails.py
@@ -5,6 +5,7 @@ from email.mime.text import MIMEText
 from argparse import ArgumentParser
 from datetime import date
 from smtplib import SMTP
+import json
 
 if __name__ == '__main__':
     print('   .')
@@ -12,7 +13,7 @@ if __name__ == '__main__':
     print(' ____.o___')
     print(' \  .    / Silvestermails')
     print('  \~~~~~/')
-    print("   '-.-'   Version 2.0")
+    print("   '-.-'   Version 2.1")
     print('     |')
     print('     |     Konstantin Weddige')
     print("   _.'._")
@@ -21,7 +22,7 @@ if __name__ == '__main__':
     parser.add_argument('-s', '--subject', nargs='?', default='Ein frohes neues Jahr {0}'.format(date.today().year))
     parser.add_argument('-f', '--sender', nargs='?', default='ich@domain.tld')
     parser.add_argument('-b', '--body', nargs='?', default='body.txt')
-    parser.add_argument('-r', '--recipients', nargs='?', default='recipients.txt')
+    parser.add_argument('-r', '--recipients', nargs='?', default='recipients.json')
     args = parser.parse_args()
 
     file = open(args.body, 'r', encoding='utf-8')
@@ -30,17 +31,20 @@ if __name__ == '__main__':
 
     recipients = open(args.recipients, 'r')
     try:
-        for recipient in recipients:
-            to, name = recipient.split('\t', 2)
-            mail = MIMEText(body.format(name=name), 'plain', 'utf-8')
-            mail['To'] = to
+        for recipient in json.load(recipients):
+            try:
+                mail = MIMEText(body.format(**recipient), 'plain', 'utf-8')
+            except KeyError as e:
+                print('Dein Template verwendet eine nicht definierte Variable.')
+                break
+            mail['To'] = recipient['email']
             mail['From'] = args.sender
             mail['Subject'] = args.subject
 
             print(mail.as_string())
-            
+
             s = SMTP('localhost')
-            s.sendmail(args.sender, [to], mail.as_string())
+            s.sendmail(args.sender, [recipient['email']], mail.as_string())
             s.quit()
     except OSError: #Python 3.2 kennst ConnectionRefusedError nicht
         print('Du scheinst keinen lokalen Mailserver zu haben.')
